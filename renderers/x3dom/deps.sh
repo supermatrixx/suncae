0cc3655b8094ff74bca26602d9e01415db6060ea
gtheler <jeremy@seamplex.com>
2024-12-02 07:31:40 -0300

    download x3dom with latest fix

--- renderers/x3dom/deps.sh
+++ renderers/x3dom/deps.sh
@@ -4,20 +4,25 @@
 x3dom_version=1.8.4-dev
 
 echo -n "renderers/x3dom: x3dom.js & x3dom.css... "
-if [ ! -e renderers/x3dom/x3dom.js ]; then
+if [ $force = 1 ] || [ ! -e renderers/x3dom/x3dom.js ]; then
   cd deps
   x3dom_tarball=x3dom-${x3dom_version}
  
-  if [ ! -e ${x3dom_tarball}.zip ]; then
+#   if [ ! -e ${x3dom_tarball}.zip ]; then
 #     wget -c https://www.x3dom.org/download/${x3dom_version}/${x3dom_tarball}.zip
-    wget -c https://www.x3dom.org/download/dev/${x3dom_tarball}.zip
-  fi
-  if [ ! -d x3dom ]; then
-    mkdir -p x3dom
-    unzip ${x3dom_tarball}.zip -d x3dom
-  fi  
-  cp x3dom/x3dom.js  ../renderers/x3dom
-  cp x3dom/x3dom.css ../renderers/x3dom
+#     wget -c https://www.x3dom.org/download/dev/${x3dom_tarball}.zip
+#   fi
+#   if [ ! -d x3dom ]; then
+#     mkdir -p x3dom
+#     unzip ${x3dom_tarball}.zip -d x3dom
+#   fi  
+#   cp x3dom/x3dom.js  ../renderers/x3dom
+#   cp x3dom/x3dom.css ../renderers/x3dom
+
+  wget -c https://andreasplesch.github.io/x3dom/dist/x3dom.js
+  cp x3dom.js  ../renderers/x3dom
+  wget -c https://andreasplesch.github.io/x3dom/dist/x3dom.css
+  cp x3dom.css ../renderers/x3dom
   
   cd ../uxs/faster-than-quick/js
   if [ ! -e x3dom.js ]; then
